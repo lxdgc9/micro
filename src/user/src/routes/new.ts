01f43de49bf7fef59a9d5f3539db0eec815256e5
DANH PHI LONG <longsf9@gmail.com>
2023-02-21 16:05:26 +0700

    update

--- src/user/src/routes/new.ts
+++ src/user/src/routes/new.ts
@@ -1,4 +1,5 @@
-import { Request, Response, Router } from "express";
+import { BadRequestError } from "@gdvn-longdp/common";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserCreationSuccessPublisher } from "../events/publishers/user-creation-success-publisher";
 import { Profile } from "../models/profile";
 import { User } from "../models/user";
@@ -7,34 +8,39 @@ import { natsWrapper } from "../nats-wrapper";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response, next: NextFunction) => {
   const { fullName, username, phone, email, password } = req.body;
+  try {
+    const profile = Profile.build({
+      baseInfo: {
+        fullName,
+        phone,
+        email,
+      },
+    });
+    await profile.save();
 
-  const profile = Profile.build({
-    baseInfo: {
-      fullName,
-      phone,
-      email,
-    },
-  });
-  const user = User.build({ profile: profile.id });
-  await profile.save();
-  await user.save();
+    const user = User.build({ profile: profile.id });
+    await user.save();
 
-  res.status(201).send(user);
+    const userFilter = UserFilter.build({
+      userId: user.id,
+      fullName: profile.baseInfo.fullName,
+      phone: profile.baseInfo.phone,
+    });
+    await userFilter.save();
 
-  new UserCreationSuccessPublisher(natsWrapper.client).publish({
-    username,
-    password,
-    userId: user.id,
-  });
+    res.status(201).send(user);
 
-  const userFilter = UserFilter.build({
-    userId: user.id,
-    fullName: profile.baseInfo.fullName,
-    phone: profile.baseInfo.phone,
-  });
-  await userFilter.save();
+    new UserCreationSuccessPublisher(natsWrapper.client).publish({
+      username,
+      password,
+      userId: user.id,
+    });
+  } catch (err) {
+    console.log(err);
+    next(new BadRequestError("Create new user failure"));
+  }
 });
 
 export { router as newUserRouter };
